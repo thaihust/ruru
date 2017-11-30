944e5e16bdb4a3e00c84f323ec64ba0dad0881a3
thaihust <thaiph.hust@gmail.com>
2017-11-30 16:29:08 +0700

    update run script

---
+++ installer/3_run_services.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source env.sh
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+docker run -d -p 127.0.0.1:8086:8086 -p 8083:8083 -e INFLUXDB_ADMIN_ENABLED=true --name influxdb -v ../influxdb:/var/lib/influxdb influxdb
+sleep 3
+INFLUXDB_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' influxdb)
+
+docker run -d -p 3001:3001 -p 3000:3000 -p 127.0.0.1:6080:6080 -e MAPBOX_TOKEN='${MAPBOX_TOKEN}' -e INFLUX_HOST='${INFLUXDB_IP}' --name frontend frontend 
+sleep 3
+FRONTEND_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' frontend)
+
+docker run -d -p 5502:5502 -p 5503:5503 -e http_proxy= -e https_proxy= --name analytics analytics ./analytics --bind tcp://0.0.0.0:5502 tcp://0.0.0.0:5503 --publish tcp://${FRONTEND_IP}:6080 --influx http://${INFLUXDB_IP}:8086/write?db=rtt
+sleep 3
+
+docker run -d -p 4000:3000 -v ../grafana:/var/lib/grafana -e "GF_SECURITY_ADMIN_PASSWORD=${GRAFANA_PASS}" --name grafana grafana/grafana 
--- installer/env.sh
+++ installer/env.sh
@@ -1,5 +1,6 @@
 export VIF=ens9
 export MAPBOX_TOKEN="pk.eyJ1IjoidGhhaWh1c3QiLCJhIjoiY2phZ3BwbHppMWd2ZjMzbGQwZXpsY2J4dyJ9.o4tRfpvrBa8c2HGyV2SwpQ"
+export GRAFANA_PASS="welcome123"
 export PROXY="http://192.168.5.8:3128/"
 export http_proxy="${PROXY}"
 export https_proxy="${PROXY}"
